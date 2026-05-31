9a8637038c28bae9a8dbec7209a47aa94f718c53
khanhasanur111 <dev.khanuu@gmail.com>
2026-05-31 17:48:09 +0600

    factorial last digit

---
+++ factorial_last_digit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class factorial_last_digit {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            int x=sc.nextInt();
+            int fact=1;
+            for (int j=1;j<=x;j++){
+                fact=fact*j;
+            }
+            System.out.println(fact);
+        }
+    }
+}
