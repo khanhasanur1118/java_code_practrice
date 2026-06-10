10ac65bcb63fd3800bf21a90dd2943cfe85eaffc
khanhasanur111 <dev.khanuu@gmail.com>
2026-06-10 22:05:32 +0600

    new unlimited gussing game

---
+++ extendSwitch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class extendSwitch {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a day:");
+        String day = sc.nextLine();
+
+        switch(day){
+            case "satarday" -> System.out.println("its a weekend");
+            case "sunday" -> System.out.println("its a weekday");
+            case "monday" -> System.out.println("its a weekday");
+            case "tuesday" -> System.out.println("its a weekday");
+            case "wednesday" -> System.out.println("its a weekday");
+            case "thursday" -> System.out.println("its a weekday");
+            case "friday" -> System.out.println("its a weekend");
+            default -> System.out.println(day+" its a not even a day");
+        }
+    }
+}
