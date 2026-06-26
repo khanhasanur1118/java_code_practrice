1b57b98a070bce6b37bdc599c8cdc3258323e744
khanhasanur111 <dev.khanuu@gmail.com>
2026-06-26 22:24:02 +0600

    loop

---
+++ oop_learning/fizz_bazz.java
@@ -0,0 +1,27 @@
+package oop_learning;
+
+public class fizz_bazz {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if (i%3==0 && i%5==0) {
+                System.out.println(i+"is fizzbuzz,");
+            }
+            else if (i%3==0 ) {
+                System.out.println(i+"is fizz,");
+            }
+            else if (i%5==0) {
+                System.out.println(i+"is buzz,");
+            }
+            else {
+                System.out.println(i+"");
+            }
+        }
+        int j=1;
+        while (j<15){
+            if (j%2==0) {
+                System.out.println(j+"is even number");
+            }
+            j++;
+        }
+    }
+}
