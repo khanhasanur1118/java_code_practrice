e2e595c6d40cb1caf92c009bbd805eeefb7d0d01
khanhasanur111 <dev.khanuu@gmail.com>
2026-06-27 02:36:02 +0600

    new

---
+++ oop_learning/exception_handeling.java
@@ -0,0 +1,23 @@
+package oop_learning;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class exception_handeling {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.print("Enter a student ID: ");
+            int studentID = input.nextInt();
+            System.out.print("Enter a student name: ");
+            String name=input.next();
+
+
+        }
+        catch(InputMismatchException e){
+            System.out.println("Invalid student ID");
+        }
+        catch(Exception e){
+            System.out.println("something went wrong");
+        }
+    }
+}
