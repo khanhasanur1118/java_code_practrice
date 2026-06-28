e1b726415ff5d878879207232e6960e63f904426
khanhasanur111 <dev.khanuu@gmail.com>
2026-06-29 01:59:27 +0600

    fresh correction ...

---
+++ oop_learning/ConstructorOverLoad.java
@@ -0,0 +1,51 @@
+package oop_learning;
+import java.util.*;
+import java.io.*;
+import static oop_learning.Taka.*;
+
+public class ConstructorOverLoad {
+    public static void main(String[] args) {
+        Taka tk=new Taka();
+        System.out.println(tk.amount);
+        Taka.show();
+    }
+}
+
+
+
+
+class Taka{
+    int amount;
+    String bankName;
+    String accType;
+
+
+    Taka(){
+        this(100000);
+        System.out.println("no 1 constructor");
+    }
+    Taka(int amount){
+        this(amount,null);
+        System.out.println("no 2 constructor");
+    }
+    Taka(int amount,String bankName){
+        this(amount,bankName,null);
+        System.out.println("no 3 constructor");
+    }
+    Taka(int amount,String bankName,String accType){
+        this.amount=amount;
+        this.bankName=bankName;
+        this.accType=accType;
+        System.out.println("main work start");
+    }
+    static void show(){
+        int a=5,b=5;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+
+        }
+    }
+}
