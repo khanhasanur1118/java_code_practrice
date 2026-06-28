cc0cd6f59ab5671a464c5e493ce8107f26e29605
khanhasanur111 <dev.khanuu@gmail.com>
2026-06-29 00:40:43 +0600

    new constructoe,this keyword learning.overload

--- oop_learning/constructor.java
+++ oop_learning/constructor.java
@@ -5,8 +5,14 @@ import java.io.*;
 public class constructor {
     static void main(String[] args) {
         Admission add=new Admission();
-        System.out.println(new StringBuilder().append(add.university).append(",").append(add.degree_lavel).append(",").append(add.seassion).append(",").append(add.scholarship).toString());
+
+        System.out.println(new StringBuilder().append(add.university).append(",").append(add.degree_lavel).toString());
+        Admission add1=new Admission("khan","Msc",2027,true);
+        System.out.println(new StringBuilder().append(add1.university).append(",").append(add1.degree_lavel).append(",").append(add1.seassion).append(",").append(add1.scholarship).toString());
     }
+
+
+
 }
 class Admission{
     String university;
@@ -17,7 +23,13 @@ class Admission{
     Admission(){
         this.university = "KFUPM";
         this.degree_lavel = "Msc";
-        this.seassion = 2027;
-        this.scholarship = true;
+       // this.seassion = 2027;
+       // this.scholarship = true;
+    }
+    Admission(String university, String degree_lavel, int seassion, boolean scholarship){
+        this.university = university;
+        this.degree_lavel = degree_lavel;
+        this.seassion = seassion;
+        this.scholarship = scholarship;
     }
 }
