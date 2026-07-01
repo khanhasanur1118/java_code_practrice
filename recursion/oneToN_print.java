4edfe2433de6561b4e803a9d9c462df4c2718577
khanhasanur111 <dev.khanuu@gmail.com>
2026-07-02 03:33:45 +0600

    print assending number through recursion

--- recursion/oneToN_print.java
+++ recursion/oneToN_print.java
@@ -10,6 +10,7 @@ public class oneToN_print {
         int n2 = sc.nextInt();
         print(n1,n2);
         printReverse(1,n2);
+        anotherVersionPrint(12);
         sc.close();
     }
     static void print(int start,int end) {
@@ -23,6 +24,17 @@ public class oneToN_print {
         if(start>end)return;
         System.out.println(end);
         printReverse(1,end-1);
+    }
+    static void anotherVersionPrint(int k)throws ArithmeticException {
+        if(k==0)return;
+        int []arr=new int[k+1];
+        for(int i=1;i<=k;i++){
+            arr[i]=i;
+            System.out.print(arr[i]);
+            if (i<=arr.length-2)
+                System.out.print(", ");
+        }
+
     }
 
 }
