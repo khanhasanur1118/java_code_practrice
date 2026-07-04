858c912ca88ea00cb57e2d0013a6615395c2fb9f
khanhasanur111 <dev.khanuu@gmail.com>
2026-07-04 09:37:09 +0600

    new item

--- pattern_print/number_diamond.java
+++ pattern_print/number_diamond.java
@@ -8,7 +8,7 @@ public class number_diamond {
         int number = sc.nextInt();
         int i,j,k;
         int count=number-1;
-        for(i=1;i<=2*(number)-1;i++)
+        for(i=1;i<=2*(number)-1;i+=2)
         {
             for(k=1;k<=count;k++){
                 System.out.print(" ");
@@ -20,5 +20,32 @@ public class number_diamond {
             }
             System.out.println();
         }
+
+
+
+        count=1;
+        for(i=2*(number)-1;i>=1;i-=2)
+        {
+            if(i!=2*(number)-1)
+            {
+                for (k = 1; k <= count; k++){
+                    System.out.print(" ");
+                }
+                count++;
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+
+
+
+        sc.close();
+
+
+
+
     }
 }
