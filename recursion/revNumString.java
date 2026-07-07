2357e2ef7cb548039f04c186d89de9e2b53a99ba
khanhasanur111 <dev.khanuu@gmail.com>
2026-07-07 08:31:44 +0600

    reverse

---
+++ recursion/revNumString.java
@@ -0,0 +1,28 @@
+package recursion;
+import java.util.Scanner;
+
+public class revNumString {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int t = sc.nextInt();
+        System.out.println("enter a String--");
+        String st= sc.next();
+        System.out.println("reverse number:"+ printReverse(t,0));
+        System.out.println("reversed string--"+printReverse2(st));
+        sc.close();
+    }
+
+
+    public static int printReverse(int n,int r) {
+            if (n==0)return r;
+            return printReverse(n/10,r*10+n%10);
+
+    }
+    public static String printReverse2(String as) {
+        if (as.isEmpty())return "";
+        StringBuilder sb = new StringBuilder(as);
+        return String.valueOf(String.valueOf(sb.reverse()));
+    }
+
+}
