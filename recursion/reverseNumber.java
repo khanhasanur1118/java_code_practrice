940cf100f51433119ab45c1c3f933fade487a181
khanhasanur111 <dev.khanuu@gmail.com>
2026-07-07 06:11:31 +0600

    BigInt for power calculation

--- recursion/reverseNumber.java
+++ recursion/reverseNumber.java
@@ -1,12 +1,20 @@
 package recursion;
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class reverseNumber {
     public static void main(String[]args)
     {   Scanner sc=new Scanner(System.in);
+        System.out.println("start");
         int start=sc.nextInt();
+        System.out.println("end");
         int end=sc.nextInt();
         reverse(start,end);
+        System.out.println("Base: ");
+        long base=sc.nextLong();
+        System.out.println("exponent: ");
+        long exponent=sc.nextLong();
+        System.out.println("result:"+pows(BigInteger.valueOf(base),exponent));
         sc.close();
 
     }
@@ -19,6 +27,14 @@ public class reverseNumber {
         System.out.println(end);
         return reverse(start,end-1);
 
+    }
+
+    public static BigInteger pows(BigInteger a,long b)throws IllegalArgumentException
+    {  if(b<0){
+        throw new IllegalArgumentException("Exponent 'b' cannot be negative for integer power calculations.");
+    }
+        if(b==0) return BigInteger.ONE;
+        return a.multiply(pows(a,b-1));
     }
 
 }
